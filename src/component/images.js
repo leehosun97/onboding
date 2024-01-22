cebe9a9b089572fed2de342d20bdccf6d565b95c
hs97 <leehosun97@naver.com>
2024-01-22 11:12:05 +0900

    onbodingcode

---
+++ src/component/images.js
@@ -0,0 +1,38 @@
+import slideImage01 from '../images/slide_img_pc01.png';
+import slideImage02 from '../images/slide_img_pc02.png';
+import slideImage03 from '../images/slide_img_pc03.png';
+import slideImage04 from '../images/slide_img_pc04.png';
+import slideImage05 from '../images/slide_img_pc05.png';
+import skillImg01 from '../images/skil_img01.png';
+import skillImg02 from '../images/skil_img02.png';
+import skillImg03 from '../images/skil_img03.png';
+import skillImg04 from '../images/skil_img04.png';
+import skillImg05 from '../images/skil_img05.png';
+import skillImg06 from '../images/skil_img06.png';
+/* 모바일 */
+import MoImg01 from '../images/mo_img01.png';
+import MoImg02 from '../images/mo_img02.png';
+import MoImg03 from '../images/mo_img03.png';
+import MoImg04 from '../images/mo_img04.png';
+import MoImg05 from '../images/mo_img05.png';
+
+const images = {
+  slideImage01,
+  slideImage02,
+  slideImage03,
+  slideImage04,
+  slideImage05,
+  skillImg01,
+  skillImg02,
+  skillImg03,
+  skillImg04,
+  skillImg05,
+  skillImg06,
+  MoImg01,
+  MoImg02,
+  MoImg03,
+  MoImg04,
+  MoImg05,
+};
+
+export default images;
